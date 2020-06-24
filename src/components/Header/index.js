8112e4d03d17bb332d2c1b8f847f1a56750e28ab
sujatha <jhanavi.bhushan@gmail.com>
2020-06-23 22:55:02 -0400

    Added Header and Footer components

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <header className="header" >
+        <h1 className="right-align headerText">Employee Directory</h1>
+      </header>
+
+    )
+  }
+  
+  export default Header;
+  
