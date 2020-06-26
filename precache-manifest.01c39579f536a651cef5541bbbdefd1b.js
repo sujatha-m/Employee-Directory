0e0fdd59e682390138ab3c7bf6e661b4b0c68335
sujatha <jhanavi.bhushan@gmail.com>
2020-06-25 20:43:24 -0400

    Updates

---
+++ precache-manifest.01c39579f536a651cef5541bbbdefd1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230b63bbc208393d3219b29bdc4014b6",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "31200177be01c6f67e8d",
+    "url": "/react-employee-directory/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2f3a1a9ec582df310f0a",
+    "url": "/react-employee-directory/static/css/main.8051ea52.chunk.css"
+  },
+  {
+    "revision": "31200177be01c6f67e8d",
+    "url": "/react-employee-directory/static/js/2.0d28dbf4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-employee-directory/static/js/2.0d28dbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3a1a9ec582df310f0a",
+    "url": "/react-employee-directory/static/js/main.30cf9ec5.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
